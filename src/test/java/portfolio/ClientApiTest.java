d82edf507fee2e4c0c2e78c7f09e0e6d104af30a
minal <minal.fulzele@hmcts.net>
2020-08-27 05:27:31 +0100

    added second failing test

--- src/test/java/portfolio/ClientApiTest.java
+++ src/test/java/portfolio/ClientApiTest.java
@@ -19,6 +19,16 @@ public class ClientApiTest {
                 .compareTo(new BigDecimal(0))>0);
     }
 
+    @Test
+    void shouldReturnGBPvalueForBTCbitcoin() {
+        ClientApi clientApi = new ClientApi();
+        String bitCoinValue = clientApi.getBitCoinValue("BTC", "GBP");
+        assertNotNull(bitCoinValue);
+        assertTrue(bitCoinValue.contains("GBP"));
+        assertTrue(new BigDecimal(extractBitcoinValueFromResponse(bitCoinValue))
+                .compareTo(new BigDecimal(0))>0);
+    }
+
     private String extractBitcoinValueFromResponse(String bitCoinValue) {
         //Example response format--> {"EUR":324.12}
         return bitCoinValue.substring(bitCoinValue.indexOf(":") + 1, bitCoinValue.length() - 1);
