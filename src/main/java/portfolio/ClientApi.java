893c00a9c34ec5874542ffb99541f389251939aa
minal <minal.fulzele@hmcts.net>
2020-08-27 05:37:59 +0100

    passed the two failing test : added code to interact with the free rest api. We know at this point that we have to interact with the endpoint, so rather than following the TDD strategies like trasformation priority premise - we quickly added rest client code to pass the tests

--- src/main/java/portfolio/ClientApi.java
+++ src/main/java/portfolio/ClientApi.java
@@ -1,8 +1,35 @@
 package portfolio;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
+
 public class ClientApi {
 
-    public String getBitCoinValue(String bitcoin, String currency) {
-        return "{\"EUR\":324.12}";
+    public String getBitCoinValue(String bitcoin, String currency) throws IOException, InterruptedException {
+
+        String uri = new StringBuilder()
+                .append("https://min-api.cryptocompare.com/data/price?fsym=")
+                .append(bitcoin)
+                .append("&tsyms=")
+                .append(currency)
+                .toString();
+
+        final var httpClient = HttpClient.newBuilder().version(HttpClient.Version.HTTP_2).build();
+
+        final var httpRequest = HttpRequest.newBuilder()
+                .uri(URI.create(uri))
+                .timeout(Duration.ofMinutes(1))
+                .header("Content-Type", "application/json")
+                .GET()
+                .build();
+
+        HttpResponse<String> response = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
+
+        return response.body();
+
     }
 }
--- src/test/java/portfolio/ClientApiTest.java
+++ src/test/java/portfolio/ClientApiTest.java
@@ -3,6 +3,7 @@ package portfolio;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.io.IOException;
 import java.math.BigDecimal;
 
 import org.junit.jupiter.api.Test;
@@ -10,23 +11,25 @@ import org.junit.jupiter.api.Test;
 public class ClientApiTest {
 
     @Test
-    void shouldReturnEURvalueForETHbitcoin() {
+    void shouldReturnEURvalueForETHbitcoin() throws IOException, InterruptedException {
         ClientApi clientApi = new ClientApi();
         String bitCoinValue = clientApi.getBitCoinValue("ETH", "EUR");
         assertNotNull(bitCoinValue);
         assertTrue(bitCoinValue.contains("EUR"));
         assertTrue(new BigDecimal(extractBitcoinValueFromResponse(bitCoinValue))
-                .compareTo(new BigDecimal(0))>0);
+                .compareTo(new BigDecimal(0)) > 0);
+
     }
 
     @Test
-    void shouldReturnGBPvalueForBTCbitcoin() {
+    void shouldReturnGBPvalueForBTCbitcoin() throws IOException, InterruptedException {
         ClientApi clientApi = new ClientApi();
         String bitCoinValue = clientApi.getBitCoinValue("BTC", "GBP");
         assertNotNull(bitCoinValue);
         assertTrue(bitCoinValue.contains("GBP"));
         assertTrue(new BigDecimal(extractBitcoinValueFromResponse(bitCoinValue))
-                .compareTo(new BigDecimal(0))>0);
+                .compareTo(new BigDecimal(0)) > 0);
+
     }
 
     private String extractBitcoinValueFromResponse(String bitCoinValue) {
