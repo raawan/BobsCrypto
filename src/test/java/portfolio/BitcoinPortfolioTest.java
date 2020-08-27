531be0b70536963529860c5b9e21d17559eac225
minal <minal.fulzele@hmcts.net>
2020-08-27 11:19:06 +0100

    removed the redundant test case to reduce verbosity

--- src/test/java/portfolio/BitcoinPortfolioTest.java
+++ src/test/java/portfolio/BitcoinPortfolioTest.java
@@ -8,19 +8,6 @@ import org.junit.jupiter.api.Test;
 
 public class BitcoinPortfolioTest {
 
-    @DisplayName("Given a valid file input with one bitcoin, should return total by multiplying units to the single bitcoin value ")
-    @Test
-    public void shouldReturnTotalValueOfSinleBitcoin() {
-
-        BitcoinPortfolio bitcoinPortfolio = new BitcoinPortfolio(new ClientApi(),
-                new FileApi("src/test/resources/bobs_crypto_test_onebitcoin.txt"));
-
-        final var totalValueOfEachBitcoin = bitcoinPortfolio.calculateValueOfEachBitcoin();
-
-        assertEquals(1, totalValueOfEachBitcoin.size());
-        assertTrue(totalValueOfEachBitcoin.get("BTC").doubleValue()!=0);
-    }
-
     @DisplayName("Given a valid file input with many bitcoin, should return total for each bitcoin by multiplying units to the single bitcoin value ")
     @Test
     public void shouldReturnTotalValueOfAllBitcoinsInAFile() {
