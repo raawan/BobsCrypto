62266783354272461c4d81ab7a2e52609b2ce56d
minal <minal.fulzele@hmcts.net>
2020-08-27 05:23:45 +0100

    Added first failing test to test rest client that communicates with free API to fetch bitcoin value

---
+++ src/test/java/portfolio/ClientApiTest.java
@@ -0,0 +1,26 @@
+package portfolio;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.math.BigDecimal;
+
+import org.junit.jupiter.api.Test;
+
+public class ClientApiTest {
+
+    @Test
+    void shouldReturnEURvalueForETHbitcoin() {
+        ClientApi clientApi = new ClientApi();
+        String bitCoinValue = clientApi.getBitCoinValue("ETH", "EUR");
+        assertNotNull(bitCoinValue);
+        assertTrue(bitCoinValue.contains("EUR"));
+        assertTrue(new BigDecimal(extractBitcoinValueFromResponse(bitCoinValue))
+                .compareTo(new BigDecimal(0))>0);
+    }
+
+    private String extractBitcoinValueFromResponse(String bitCoinValue) {
+        //Example response format--> {"EUR":324.12}
+        return bitCoinValue.substring(bitCoinValue.indexOf(":") + 1, bitCoinValue.length() - 1);
+    }
+}
