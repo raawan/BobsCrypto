67d324aafd9b6ea50979a2b5b2451ea203c3f553
minal <minal.fulzele@hmcts.net>
2020-08-27 11:07:52 +0100

    added another test passing input to a file containing many bitcoins - it passed with existing code

--- src/test/java/portfolio/BitcoinPortfolioTest.java
+++ src/test/java/portfolio/BitcoinPortfolioTest.java
@@ -3,13 +3,14 @@ package portfolio;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class BitcoinPortfolioTest {
 
-
+    @DisplayName("Given a valid file input with one bitcoin, should return total by multiplying units to the single bitcoin value ")
     @Test
-    public void shouldReturnTotalValueOfEachBitcoin() {
+    public void shouldReturnTotalValueOfSinleBitcoin() {
 
         BitcoinPortfolio bitcoinPortfolio = new BitcoinPortfolio(new ClientApi(),
                 new FileApi("src/test/resources/bobs_crypto_test_onebitcoin.txt"));
@@ -19,5 +20,22 @@ public class BitcoinPortfolioTest {
         assertEquals(1, totalValueOfEachBitcoin.size());
         assertTrue(totalValueOfEachBitcoin.get("BTC").doubleValue()!=0);
     }
+
+    @DisplayName("Given a valid file input with many bitcoin, should return total for each bitcoin by multiplying units to the single bitcoin value ")
+    @Test
+    public void shouldReturnTotalValueOfAllBitcoinsInAFile() {
+
+        BitcoinPortfolio bitcoinPortfolio = new BitcoinPortfolio(new ClientApi(),
+                new FileApi("src/test/resources/bobs_crypto_test.txt"));
+
+        final var totalValueOfEachBitcoin = bitcoinPortfolio.calculateBitcoinTotalValue(bitcoinPortfolio.getFileApi().getLines());
+
+        assertEquals(3, totalValueOfEachBitcoin.size());
+        assertTrue(totalValueOfEachBitcoin.get("BTC").doubleValue()!=0);
+        assertTrue(totalValueOfEachBitcoin.get("ETH").doubleValue()!=0);
+        assertTrue(totalValueOfEachBitcoin.get("ATOM").doubleValue()!=0);
+
+    }
+
 
 }
